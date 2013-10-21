bb8c8edde14208038d626600aa9e303dd6c2d57f
Poojan Jhaveri <poojanjhaveri@hotmail.com>
2013-10-21 11:02:49 -0700

    Added custom image icon and info window for every marker. Templating also done.

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 // JavaScript Document
-console.log("Poojan");
+
+var templateString = $('#concerts-template').html();
+var templateFunction = Handlebars.compile(templateString);
+
 var map = new google.maps.Map(document.getElementById('map'),{
-	zoom:2,
+	zoom:1,
 	center:new google.maps.LatLng(36.05178307933835,42.49737373046878),
 	mapTypeId:google.maps.MapTypeId.ROADMAP
 	});
 		
 
 $(window).load(function () {
-        
-jsonRequest("http://api.eventful.com/json/events/search?c=music&app_key=gVsKKsfS5PsGS6sg&page_number=1&date=Future&keywords=kanye-west&callback=processJSONP"); // Request
+// Send the Request        
+jsonRequest("http://api.eventful.com/json/events/search?c=music&app_key=gVsKKsfS5PsGS6sg&page_number=1&date=Future&keywords=kanye-west&callback=processJSONP");
 });
 		
 /*
@@ -27,23 +30,40 @@ Callback function after the request
 */
 function processJSONP(data)
 {
-	console.log("hie");
-	var $content = $('#content');			// selector caching
+	var $content = $('#sidebar');			// selector caching
 	$content.html("");						// Erase the content at beginning of every json Request
-	console.log(data);
+//	console.log(data);
 	if(data.events.length==0)				// Results count is zero. So no data obtained
 	{
 		$content.html("No events found");		// No vaid concerts found
 		}
 	else
-	{
-	  for(var i=0;i<data.events.event.length;i++)
+	{		
+	  for(var i=0;i<data.events.event.length;i++)																// Iterate through list of events
 	  {
-		  console.log(data.events.event[i]);
-	//	 var htmlWithData = templateFunction(data.events.event[i]);	 // pass the data to the function so that it compiles
-	//	 $content.append(htmlWithData);					// Append the data to existing div
+		  $thisevent = data.events.event[i];																	// Get the current event
+		  console.log($thisevent);
+		  var marker = new google.maps.Marker({	
+                            position:new google.maps.LatLng($thisevent.latitude, $thisevent.longitude),			// Get the latitude and longitude
+							icon:"icon.png",																	// Custom image icon
+							title:"Date : "+$thisevent.start_time+"</br><a href="+$thisevent.url+">MORE</a>",	// Set Date and URL
+                            });
+							
+				var infowindow = new google.maps.InfoWindow();		
+                marker.setMap(map);
+				
+				google.maps.event.addListener(marker,'click',function(){	// On click event listener
+				infowindow.setContent(this.title);							// get the title property of marker
+    			infowindow.open(map,this);									// Open infowindow associated with this marker
+				});
+				
+		  
+		 var htmlWithData = templateFunction($thisevent);	 	// pass the data to the function so that it compiles
+		 $content.append(htmlWithData);							// Append the data to existing div
 	  }
 	}
 
    
 }
+
+
